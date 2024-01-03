a90bf7756ca8565321bd84d1bfae46e026da1680
muh11 <naufalaswinana@gmail.com>
2024-01-03 15:06:10 +0700

    part 3

--- routes/web.php
+++ routes/web.php
@@ -14,58 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        'name' => 'Muh11',
+        'role' => 'admin',
+        'buah' => ['pisang','durian', 'jeruk','lengkeng']
+    ]);
 });
-
-Route ::get('/about', function(){
-    return 'whatever';
-});
-
-Route:: get('/contact',function(){
-    return view ('contact',['name' => 'cara Nomuh', 'phone' => '0812314292'] );
-});
-
-Route:: get('/product', function(){
-    return 'product';
-});
-
-// Route:: get('/product/{id}', function($id){
-//     return 'ini adalah product dengan id ' .$id;
-// });
-
-Route::get('/product/{id}', function($id){
-    return view ('product.detail', ['id' => $id]);
-});
-
-// Route::redirect('/contact', '/about');
-// Route:: view('/contact', 'contact', ['name' => 'cara Nomuh', 'phone' => '0812314292']);
-
-
-
-Route::prefix('administrator')->group(function () {
-    Route::get('/profil-admin', function(){
-        return 'profil admin';
-    });
-    
-    Route::get('/about-admin', function(){
-        return 'about admin';
-    });
-    
-    Route::get('/contact-admin', function(){
-        return 'contact admin';
-    });
-    
-    Route::get('/profil-admin2', function(){
-        return 'profil admin2';
-    });
-    
-    Route::get('/about-admin2', function(){
-        return 'about admin2';
-    });
-    
-    Route::get('/contact-admin2', function(){
-        return 'contact admin2';
-    });
-});
-
-//dafadfa
