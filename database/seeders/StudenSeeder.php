d8bcf9254bb897dfcfe6257922128c3f12a94a62
muh11 <naufalaswinana@gmail.com>
2024-01-08 16:36:39 +0700

    Database faker

--- database/seeders/StudenSeeder.php
+++ database/seeders/StudenSeeder.php
@@ -17,26 +17,30 @@ class StudenSeeder extends Seeder
      */
     public function run()
     {
-        Schema:: disableForeignKeyConstraints();
-        Student::truncate();
-        Schema::enableForeignKeyConstraints();
-    
-        $data=[
-            ['name'=>'Naufal','gender'=>'L', 'nis'=>'0101001','class_id'=>2],
-            ['name'=>'Sara','gender'=>'P', 'nis'=>'0101002','class_id'=>2],
-            ['name'=>'Ari','gender'=>'L', 'nis'=>'0101003','class_id'=>1],
-            ['name'=>'Yanto','gender'=>'L', 'nis'=>'0101004','class_id'=>3]
-        ];
 
-        foreach($data as $value){
-            Student::insert([
-                'name' =>$value['name'],
-                'gender'=>$value['gender'],
-                'nis'=>$value['nis'],
-                'class_id'=>$value['class_id'],
-                'created_at'=> Carbon::now(),
-                'updated_at' => Carbon::now()
-            ]);
-        }
-    }
+
+    //     Schema:: disableForeignKeyConstraints();
+    //     Student::truncate();
+    //     Schema::enableForeignKeyConstraints();
+    
+    //     $data=[
+    //         ['name'=>'Naufal','gender'=>'L', 'nis'=>'0101001','class_id'=>2],
+    //         ['name'=>'Sara','gender'=>'P', 'nis'=>'0101002','class_id'=>2],
+    //         ['name'=>'Ari','gender'=>'L', 'nis'=>'0101003','class_id'=>1],
+    //         ['name'=>'Yanto','gender'=>'L', 'nis'=>'0101004','class_id'=>3]
+    //     ];
+
+    //     foreach($data as $value){
+    //         Student::insert([
+    //             'name' =>$value['name'],
+    //             'gender'=>$value['gender'],
+    //             'nis'=>$value['nis'],
+    //             'class_id'=>$value['class_id'],
+    //             'created_at'=> Carbon::now(),
+    //             'updated_at' => Carbon::now()
+    //         ]);
+    //     }
+
+        Student::factory()->count(20)->create();
+         }
 }
